31ea68df1b7cf38d0371e92a0c760e3dd1c95c02
xwagxm <123276427+xwagxm@users.noreply.github.com>
2026-09-25 20:40:36 +0800

    dr.com认证openwrt自动脚本

---
+++ login.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ACCOUNT="’À∫≈%40telecom"
+UPASS="√‹¬Î"
+curl -s "http://172.16.254.3/drcom/login?callback=dr1003&DDDDD=${ACCOUNT}&upass=${UPASS}&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.2&v=5337&lang=zh"
